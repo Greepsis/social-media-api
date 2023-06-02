5504a81cbd16847d27c60ae03b968b429b642367
Greepsis <anya200626@gmail.com>
2023-06-02 14:53:24 +0300

    Add test task

---
+++ socialmedia/RestService/src/test/resources/create-correct-post.sql
@@ -0,0 +1,3 @@
+insert into posts(post_id, image, post_text, publication_date_time, title, user_id) values
+('a8a62b2b-1eb7-48b2-973f-f341c1ab5515','0728584b-ed74-4a03-9cda-57b191c99e1e.jpg',
+'I from Germany','2023-06-02 13:17:04.857281','Germany','ef6b50da-44f5-4330-8320-3e2f35da27a7');
